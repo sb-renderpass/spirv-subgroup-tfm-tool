b446ac13bae971f34b59dac3c4238d2af671268f
Shraman (Shaun) <shramanb.dev@gmail.com>
2023-04-18 19:14:49 -0400

    Use pathlib to process paths

--- run.py
+++ run.py
@@ -1,61 +1,64 @@
 import argparse
+import pathlib
 import subprocess
 
 BEFORE = 'before'
 AFTER  = 'after'
 
-def run(path, verbose, validate, optimize, show):
+def run(filename, verbose, validate, optimize, show):
+    path = filename.parent
+
     subprocess.run([
         'glslc',
-        f'{path}/{BEFORE}.comp',
+        f'{filename}',
         '--target-env=vulkan1.3',
         '--target-spv=spv1.6',
-        '-o', f'{path}/{BEFORE}.spv'],
+        '-o', f'{path}/{BEFORE}.comp.spv'],
         check=True)
 
     subprocess.run([
         'spirv-dis',
-        f'{path}/{BEFORE}.spv',
-        '-o', f'{path}/{BEFORE}.spvasm'],
+        f'{path}/{BEFORE}.comp.spv',
+        '-o', f'{path}/{BEFORE}.comp.spvasm'],
         check=True)
 
     subprocess.run([
         'python',
         'main.py',
-        '-i', f'{path}/{BEFORE}.spvasm',
-        '-o', f'{path}/{AFTER}.spvasm',
+        '-i', f'{path}/{BEFORE}.comp.spvasm',
+        '-o', f'{path}/{AFTER}.comp.spvasm',
         '--verbose' if verbose else '--no-verbose'],
         check=True)
 
     subprocess.run([
         'spirv-as',
-        f'{path}/{AFTER}.spvasm',
-        '-o', f'{path}/{AFTER}.spv'],
+        f'{path}/{AFTER}.comp.spvasm',
+        '-o', f'{path}/{AFTER}.comp.spv'],
         check=True)
 
     if validate:
         subprocess.run([
             'spirv-val',
-            f'{path}/{AFTER}.spv'],
+            f'{path}/{AFTER}.comp.spv'],
             check=True)
 
     subprocess.run([
         'spirv-as',
-        f'{path}/{AFTER}.spvasm',
-        '-o', f'{path}/{AFTER}.spv'],
+        f'{path}/{AFTER}.comp.spvasm',
+        '-o', f'{path}/{AFTER}.comp.spv'],
         check=True)
 
     if optimize:
         subprocess.run([
             'spirv-opt',
-            f'{path}/{AFTER}.spv',
-            '-o', f'{path}/{AFTER}.spv',
+            f'{path}/{AFTER}.comp.spv',
+            '-o', f'{path}/{AFTER}.comp.spv',
             '-O', '--skip-validation'],
         check=True)
 
     subprocess.run([
         'spirv-cross',
-        f'{path}/{AFTER}.spv',
+        f'{path}/{AFTER}.comp.spv',
         '--output', f'{path}/{AFTER}.comp',
         '--vulkan-semantics'],
         check=True)
@@ -68,7 +71,7 @@ def run(path, verbose, validate, optimize, show):
 
 def main():
     parser = argparse.ArgumentParser('SPIRV subgroup transform')
-    parser.add_argument('path', type=str)
+    parser.add_argument('filename',   type=pathlib.Path)
     parser.add_argument('--verbose',  action=argparse.BooleanOptionalAction, default=False)
     parser.add_argument('--validate', action=argparse.BooleanOptionalAction, default=False)
     parser.add_argument('--optimize', action=argparse.BooleanOptionalAction, default=True)
@@ -76,7 +79,7 @@ def main():
     args = parser.parse_args()
 
     try:
-        run(args.path, args.verbose, args.validate, args.optimize, args.show)
+        run(args.filename, args.verbose, args.validate, args.optimize, args.show)
     except subprocess.CalledProcessError as exc:
         print(f'ERROR: {exc}')
 
